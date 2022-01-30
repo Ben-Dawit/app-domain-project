641f539d476d62d88360136f1ce9710eaa2b6edc
adighiero1 <86569792+adighiero1@users.noreply.github.com>
2022-01-30 06:26:28 -0500

    class design

---
+++ .vs/app class/app class/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace app_class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+        }
+    }
+
+    public abstract class user{
+        public bool active=true;
+        public string user_name;
+       public  string password;
+
+}
+    public class administrator : user {
+
+        public administrator(string user_name,string password) {
+            this.user_name = user_name;
+            this.password = password;
+        }
+
+        public accountant make_accountant() {
+
+            Console.WriteLine("please enter the user name");
+            string ID = Console.ReadLine();
+            Console.WriteLine("please enter the password");
+            string pass = Console.ReadLine();
+
+            return new accountant(ID,pass);
+        }
+
+        public manager make_manger() {
+
+            Console.WriteLine("please enter the user name");
+            string ID = Console.ReadLine();
+            Console.WriteLine("please enter the password");
+            string pass = Console.ReadLine();
+
+            return new manager(ID, pass);
+
+        }
+        
+    }
+
+    public class manager : user {
+        public manager() { }
+        public manager(string user_name, string password)
+        {
+            this.user_name = user_name;
+            this.password = password;
+            
+        }
+    }
+
+    public class accountant : user {
+
+        public accountant() { }
+        public accountant(string user_name,string password ) {
+            this.user_name = user_name;
+            this.password = password;
+            
+        }
+    }
+}
+
+
